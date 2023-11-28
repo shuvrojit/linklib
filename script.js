094ad1a5ae762e6d75ec9b51789a0d87f91d1edd
Shuvrojit Biswas <jsuvro17@gmail.com>
2023-11-29 00:02:17 +0600

    Implement checker for if url already in storage

--- script.js
+++ script.js
@@ -7,50 +7,58 @@ const saveTab = document.querySelector("#save-tab")
 getFromLocalStorage()
 showLinks()
 
-function saveToLocalStorage () {
-  localStorage.setItem("urls", JSON.stringify(urls))
+function saveToLocalStorage() {
+    localStorage.setItem("urls", JSON.stringify(urls))
 }
 
-function getFromLocalStorage () {
-  const savedUrls = JSON.parse(localStorage.getItem("urls"))
-  if (savedUrls) {
-    savedUrls.forEach((i) => urls.push(i))
-  }
+function getFromLocalStorage() {
+    const savedUrls = JSON.parse(localStorage.getItem("urls"))
+    if (savedUrls) {
+        savedUrls.forEach((i) =>
+            urls.push(i)
+        )
+    }
 }
 
-function showLinks () {
-  if (urls.length > 0) {
-  urls.forEach((i) => {
-    savedLinks.innerHTML += `<li><a target="_blank" href="${
-      i}">${i} </a></li>`
- })
-  }
+function showLinks() {
+    if (urls.length > 0) {
+        urls.forEach((i) => {
+            savedLinks.innerHTML += `<li><a target="_blank" href="${i}">${i} </a></li>`
+        })
+    }
 }
 
-function saveCurrentTab () {
-  const tabUrl = document.URL
-  urls.push(tabUrl)
+function saveCurrentTab() {
+    const tabUrl = document.URL
+    if (urls.includes(tabUrl)) {
+        return
+    }
+    urls.push(tabUrl)
 }
 
-function runFunction () {
-  saveToLocalStorage()
-  savedLinks.replaceChildren()
-  showLinks()
+function runFunction() {
+    saveToLocalStorage()
+    savedLinks.replaceChildren()
+    showLinks()
 }
 
 saveUrl.addEventListener("click", (e) => {
-  e.preventDefault()
+    e.preventDefault()
 
-  const url = document.querySelector("#url")
-  if (url.value.length > 0) {
-    urls.push(url.value)
-    url.value = ""
-    runFunction()
-  }
-  })
+    const url = document.querySelector("#url")
+    if (url.value.length > 0) {
+      // TODO: show a notification if url already included
+      if (urls.includes(url.value)) {
+        return
+      }
+      urls.push(url.value)
+      url.value = ""
+      runFunction()
+    }
+})
 
 saveTab.addEventListener("click", (e) => {
-  e.preventDefault()
-  saveCurrentTab()
-  runFunction()
+    e.preventDefault()
+    saveCurrentTab()
+    runFunction()
 })
